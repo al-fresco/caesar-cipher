7af70accfa788d2b8dedacf23b51e55b3848395c
Blake <blameswalker@gmail.com>
2023-04-07 13:52:09 -0700

    Add alphabet arrays

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,1 +1,3 @@
+lower_case_alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+upper_case_alphabet = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
 def caesar_cipher(string, shift_factor)
