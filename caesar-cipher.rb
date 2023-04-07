d153be66cb5a3d9d55e96dbd4ee3586fb77c0336
Blake <blameswalker@gmail.com>
2023-04-06 20:41:36 -0700

    Create array containing alphabet characters; enumerate steps to method in pseudocode

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,1 +1,7 @@
+alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+def encrypt(string, shift_num)
+    # pass a string
+    # convert string to array
+    # convert each character to the letter of the alphabet that is 'shift_num' spaces ahead of it
+    # return encrypted string
 
