62df5263bcf3e2e621c530d818216155bd0a7bfb
Blake <blameswalker@gmail.com>
2023-04-07 14:17:56 -0700

    Redefine alphabet for readability

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,12 +1,5 @@
-lower_case_alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
-upper_case_alphabet = lower_case_alphabet.upcase
-def caesar_cipher(string, shift_factor)
-    string_chars = string.chars
-    string_chars.each do |char|
-        if lower_case_alphabet.includes?(char)
-            # do something
-        elsif upper_case_alphabet.includes?(char)
-            # do something else\
-        end
-    end
-end
+require 'pry-byebug'
+alphabet_string = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+alphabet_array = alphabet_string.chars
+binding.pry
+def caesar_cipher(string, key)
