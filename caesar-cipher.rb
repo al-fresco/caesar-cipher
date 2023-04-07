d61c5237eeec82d92d1a6969410f332d7a28aa22
Blake <blameswalker@gmail.com>
2023-04-06 20:51:13 -0700

    Change 'shift_num' to 'shift_factor'; create 'string_as_array' variable

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,9 +1,10 @@
 lower_case_alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 upper_case_alphabet = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
-def encrypt(string, shift_num)
+def encrypt(string, shift_factor)
     # pass a string
     # convert string to array
-    # convert each character to the letter of the alphabet that is 'shift_num' spaces ahead of it
+    # convert each character to the letter of the alphabet that is 'shift_factor' spaces ahead of it
     # return encrypted string
+    string_as_array = string.split('')
 
 
