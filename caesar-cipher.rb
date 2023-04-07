7d147edfde9c7a3464f6d5086b6856f4610437b4
Blake <blameswalker@gmail.com>
2023-04-07 13:55:52 -0700

    Add basic logic skeleton for checking each string character to determine if it is a letter

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,12 @@
 lower_case_alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
 upper_case_alphabet = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
 def caesar_cipher(string, shift_factor)
+    string_chars = string.chars
+    string_chars.each do |char|
+        if lower_case_alphabet.includes?(char)
+            # do something
+        elsif upper_case_alphabet.includes?(char)
+            # do something else\
+        end
+    end
+end
