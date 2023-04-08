c8c9ff86facdb4132812c27b007f774a254e1d4a
Blake <blameswalker@gmail.com>
2023-04-08 15:17:41 -0700

    Allow for alphabetical wrap-around

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -8,9 +8,17 @@ def caesar_cipher(text, shift)
     utf = text.codepoints
     encrypted_int_array = utf.map do |codepoint|
         if codepoint.between?(97, 122) # checks for lowercase letters
-            codepoint + shift
+            lowercase_shifted = codepoint + shift
+            if lowercase_shifted > 122
+                difference = lowercase_shifted - 122
+                96 + difference
+            end
         elsif codepoint.between?(65, 90) # checks for uppercase letters
-            codepoint + shift
+            uppercase_shifted = codepoint + shift
+            if uppercase_shifted > 90
+                difference = uppercase_shifted - 90
+                64 + difference
+            end
         end
     end
     encrypted_chr_array = encrypted_int_array.map do |codepoint|
