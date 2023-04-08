0da8cb2a01aa3d25a796f135b8a2ccb5bb39c260
Blake <blameswalker@gmail.com>
2023-04-08 14:37:37 -0700

    Allow text and shift factor inputs

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -5,5 +5,21 @@ alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'.chars
 #uppercase letters codepoint range: 65-90
 
 def caesar_cipher(text, shift)
-    
+    text.codepoints.map do |codepoint|
+        if alphabet.includes?(codepoint)
+            (codepoint + shift).chr
+        else
+            codepoint
+        end
+    end
 end
+
+puts "Enter text to be encrypted:"
+
+text = gets.chomp
+
+puts "Enter shift factor of encryption:"
+
+shift = gets.chomp.to_i
+
+caesar_cipher(text, shift)
