e8f094cab3048e065d625a01152053b2d648f9ec
Blake <blameswalker@gmail.com>
2023-04-07 18:08:05 -0700

    Reset file

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,5 +1,1 @@
-require 'pry-byebug'
-alphabet_string = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
-alphabet_array = alphabet_string.chars
-binding.pry
-def caesar_cipher(string, key)
+def caesar_cipher(text, shift)
