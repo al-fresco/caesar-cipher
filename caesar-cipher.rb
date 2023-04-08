99a81f26b32ebae1e9b41bc7f7e60af168b50599
Blake <blameswalker@gmail.com>
2023-04-07 18:40:50 -0700

    Change value of 'alphabet' variable from string to array

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,9 +1,9 @@
 require 'pry-byebug'
 
-alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'.chars
 #lowercase letters codepoint range: 97-122
 #uppercase letters codepoint range: 65-90
 
 def caesar_cipher(text, shift)
-
+    
 end
