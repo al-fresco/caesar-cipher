1ae4140c7b4d041d1a0c8179ca66c9febc90d686
Blake <blameswalker@gmail.com>
2023-10-26 16:30:43 -0700

    Implement case-sensitive test of #caesar_cipher

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,7 +1,11 @@
 require './lib/caesar_cipher.rb'
 
 describe "#caesar_cipher" do
-  it "returns a Caesar cipher of a given word (shifted by 1)" do
+  it "returns a Caesar cipher of a given word" do
     expect(caesar_cipher('abc')).to eql('bcd')
+  end
+
+  it "returns a case-sensitive Caesar cipher of a given word" do
+    expect(caesar_cipher('AbC')).to eql('BcD')
   end
 end
