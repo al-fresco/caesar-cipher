c606365b571bf3ee6ac6b41eb9c6a4810530ffc8
Blake <blameswalker@gmail.com>
2023-10-26 16:21:39 -0700

    Update #caesar_cipher definition with cleaner and more concise code

--- lib/caesar-cipher.rb
+++ lib/caesar-cipher.rb
@@ -1,25 +1,26 @@
-#lowercase letters codepoint range: 97-122
-#uppercase letters codepoint range: 65-90
+# lowercase letters codepoint range: 97-122
+# uppercase letters codepoint range: 65-90
 
 def caesar_cipher(text, shift = 1)
-    utf = text.codepoints
-    encrypted_int_array = utf.map do |codepoint|
-        if codepoint.between?(97, 122) # checks for lowercase letters
-            lowercase_shifted = codepoint + shift
-            if lowercase_shifted > 122
-                difference = lowercase_shifted - 122
-                96 + difference
-            end
-        elsif codepoint.between?(65, 90) # checks for uppercase letters
-            uppercase_shifted = codepoint + shift
-            if uppercase_shifted > 90
-                difference = uppercase_shifted - 90
-                64 + difference
-            end
-        end
+  shifted = text.codepoints.map do |codepoint|
+    new_codepoint = codepoint + shift
+
+    if (97..122).cover?(codepoint)
+      if new_codepoint > 122
+        96 + (new_codepoint - 122)
+      else
+        new_codepoint
+      end
+    elsif (65..90).cover?(codepoint)
+      if new_codepoint > 90
+        64 + (new_codepoint - 90)
+      else
+        new_codepoint
+      end
+    else
+      codepoint
     end
-    encrypted_chr_array = encrypted_int_array.map do |codepoint|
-        codepoint.chr
-    end
-    encrypted_chr_array.join('')
+  end
+
+  shifted.map { |codepoint| codepoint.chr }.join
 end
